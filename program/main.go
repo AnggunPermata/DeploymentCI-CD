ccd7e7395bc9f4de4da8c68eb850930b3e7d2545
AnggunPermata <anggunpermata.g789@gmail.com>
2021-08-10 21:10:39 +0700

    another enter

--- program/main.go
+++ program/main.go
@@ -12,7 +12,8 @@ func main() {
 	if port == "" {
 		port = ":80"
 	}
-	
+
+
 	e := echo.New()
 	e.GET("/", hello)
 	e.GET("/:name", helloName)
@@ -21,6 +22,7 @@ func main() {
 		fmt.Println(err)
 	}
 }
+
 
 func hello(c echo.Context) error {
 	return c.HTML(200, "<h1>Hello world</h1><p>Halo dunia, saya bahagia</p>")
